76a22f41053bf2aa4b4577a54f9601928cbef571
jon <jfnielse@umich.edu>
2020-10-28 09:49:49 -0400

    On branch develop 	modified:   getSHbasis.m

--- calibration/getSHbasis.m
+++ calibration/getSHbasis.m
@@ -1,17 +1,38 @@
-function H = getSHbasis(X,Y,Z)
-% function H = getSHbasis(X,Y,Z)
+function H = getSHbasis(X,Y,Z,ord)
+% function H = getSHbasis(X,Y,Z,ord)
 %
-% get spherical harmonics basis
+% Get spherical harmonic basis of order 'ord'
 %
 % Inputs:
-%  X/Y/Z    [nx ny nz]    x/y/z coordinate corresponding to 3D image matrix
+%  X/Y/Z    [N 1]    x/y/z coordinates at which to evaluate (discretize) the basis (cm)
+%  ord      int      SH order   
 %
 % Output:
-%   H       [nx*ny*nz 9]    SH basis, including dc (B0 offset) term
+%   H       [N 9]    SH basis, including dc (B0 offset) term
 
-x = X(:); y = Y(:); z = Z(:);
+if ~isvector(X) | ~isvector(Y) | ~isvector(Z)
+	error('X, Y, Z must be vectors');
+end
+N = numel(X);
+if numel(Y) ~= N | numel(Z) ~= N
+	error('X, Y, Z must be the same length');
+end
+if rem(ord, 1) | ord < 0
+	error('Order must be non-zero integer');
+end
 
-% from shimcal.pl: shims = ('x', 'y', 'z', 'z2', 'xy', 'zx', 'x2y2', 'zy');  
-H = [ones(numel(X),1) x y z z.^2 x.*y z.*x x.^2-y.^2 z.*y];
+x = X(:);
+y = Y(:);
+z = Z(:);
 
-return;
+switch ord
+	case 0
+		H = [ones(N,1)];
+	case 1
+		H = [ones(N,1) x y z ];
+	case 2
+		H = [ones(N,1) x y z z.^2 x.*y z.*x x.^2-y.^2 z.*y];
+	otherwise
+		error(('Order > 2 not supported'));
+end
+
