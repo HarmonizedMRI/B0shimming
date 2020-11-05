44542fb0f8d710518a1c45dcfdf05b8393cb2493
jon <jfnielse@umich.edu>
2020-11-05 18:01:26 -0500

    On branch gradloss 	modified:   main.m

--- gradloss/main.m
+++ gradloss/main.m
@@ -1,8 +1,14 @@
+% signal loss model is sinc(dx*g)
+% dx = voxel size (cm)
+% g = B0 gradient (Hz/cm)
+
+% synthesize g
 x = -10:0.05:10;  % fine grid
-f0 = x.^2;  % field map
+dx = x(2)-x(1);
+f0 = x.^2;        % field map
 g = diff(f0)/dx;  % cycles/cm (Hz/cm)
 
-dx = 0.3;   % voxel size (cm)
-
+% plot resulting signal loss in voxel of size dx
+dx = 0.2;   % voxel size (cm)
 subplot(121); plot(x(2:end),g);
 subplot(122); plot(x(2:end), sinc(dx*g))
