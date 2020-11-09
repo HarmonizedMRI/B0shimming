3b3d68752d97fc65523f93e7865cc4e2022be4a3
jon <jfnielse@umich.edu>
2020-11-08 21:36:20 -0500

    On branch gradloss 	modified:   signalloss.jl

--- gradloss/signalloss.jl
+++ gradloss/signalloss.jl
@@ -1,8 +1,6 @@
 using LinearAlgebra
 using Plots
 
-include("getexample.jl")
-
 """
    function signalloss(r, g, Δ, A, Δs, te)
 
@@ -88,56 +86,5 @@ function signalloss(str::String)
 		df[i] = g[i][1]
 	end
 	display(plot(df,f))
-
-end
-
-"""
-   function cost(r, g, Δ, A, Δs, te, w)
-
-Calculate weighted cost.
-See signalloss.jl
-
-# Inputs
-- `r`   length-N vector of voxel positions (each entry is a length-3 vector)
-- `g`   length-N vector of B0 field gradients (each entry is a length-3 vector)
-- `Δ`, `A`, `Δs`, `te`: see signalloss.jl
-- `w`   length-N vector of weights
-"""
-function cost(
-	r::Vector{Vector{Float64}}, 
-	g::Vector{Vector{Float64}}, 
-	Δ::Vector{Float64}, 
-	A::Array{Float64,2}, 
-	Δs::Vector{Float64},
-	te::Float64, 
-	w::Vector{Float64})
-
-	#f = map((r,g) -> signalloss(r,g,Δ,A,Δs,te), r, g)  
-	f = signalloss(r,g,Δ,A,Δs,te)  
-
-	return norm((1 .- f).*w)/sqrt(length(f))
-end
-
-
-"""
-	cost(str::String)
-
-Test function.
-"""
-function cost(str::String)
-
-	(r,g,Δ,A,Δs,te) = getexample()
-
-	N = length(r)    # number of voxels
-
-	# spatial weights
-	w = Vector{Float64}(undef,N)
-	for i in 1:N
-		w[i] = 1.; #i/N
-	end
-
-   @time f = cost(r, g, Δ, A, Δs, te, w)
-
-	print("N: $N, f: $f \n")
 
 end
