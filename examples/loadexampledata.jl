d511d063af3a211ae2c3b1163785542ebaedc4f9
jon <jfnielse@umich.edu>
2020-11-10 10:05:06 -0500

    On branch gradloss 	modified:   createexampledata.m 	modified:   loadexampledata.jl

--- examples/loadexampledata.jl
+++ examples/loadexampledata.jl
@@ -1,69 +1,11 @@
-% demoWLS.m
-%
-% Demonstrate the proposed B0 shimming workflow using a synthesized baseline fieldmap f0.
-% 
-% Goal is to calculate shim setting changes s that minimize the weighted RMS B0 inhomogeneity.
-% Provided here as an example; other loss functions may be more useful depending on the application.
+# Get example data generated with createexampledata.m
 
-addpath ..   % path to +shim package
+using JLD, MAT
 
+matf = matread("exampledata.mat");
+A = matf["A"];
+f0 = matf["f0"];
+fov = vec(matf["fov"]);
+mask = matf["mask"];
 
-%% Calculate calibration matrix A
-% This only needs to be done once per scanner
-
-% load calibration data
-load Ffull                             % [nx ny nz 8]  (does not include DC offset term)
-[nx, ny, nz, nShim] = size(Ffull);
-FOV = 19.8*[1 1 1];   % cm
-
-% generate mask
-mask = sum(abs(Ffull),4) > 10;         % [nx ny nz]     
-N = sum(mask(:));
-
-% create F by applying mask 
-F = zeros(N, nShim);
-for ii = 1:nShim
-	tmp = Ffull(:,:,:,ii);
-	F(:,ii) = tmp(mask); 
-end
-
-% specify shim amplitudes (differences) used to obtain F (hardware units)
-S = diag([20*ones(1,3) 100*ones(1,5)]);  % do not include DC term. 2nd order amp units = 10 mA
-
-% Get spherical harmonic basis evaluated at the same N spatial locations as F
-[X,Y,Z] = shim.getgrid(nx,ny,nz,FOV);
-H = shim.getSHbasis(X(mask),Y(mask),Z(mask));   % [N 9]
-
-% Calculate calibration matrix A
-% F and S do NOT include the DC term, i.e., F: [N 8], S: [8 8]
-A = shim.getcalmatrix(F, H, S)
-
-
-%%  Perform 2nd order shimming using A
-
-% synthesize noisy fieldmap f0 = [N 1]
-nx = 60; ny = 60; nz = 20;
-FOV = [20 20 8];   % cm
-[X,Y,Z] = shim.getgrid(nx,ny,nz,FOV);
-H = shim.getSHbasis(X(:),Y(:),Z(:));   % [N 9]
-s(1:4) = 10*randn([1 4]);
-s(5:9) = 20*randn([1 5]);
-R = sqrt(X(:).^2+Y(:).^1.5);
-f0 = 10*H*A*s(:)./(10+R.^1.5);
-f0 = f0 + randn(size(f0))*max(f0(:))/10;
-
-% get shim amplitudes 'shat' that minimize RMS fieldmap
-N = size(f0,1);
-W = diag_sp(ones(N,1));
-shat = -(W*H*A)\(W*f0(:));    % [9 1]. NB! May need to be rounded before applying settings on scanner.
-
-% compare baseline and predicted fieldmaps
-f = f0 + H*A*shat;
-f = reshape(f, [nx ny nz]);
-f0 = reshape(f0, [nx ny nz]);
-im(cat(1, f0, f)); 
-title(sprintf('fieldmaps: original (left in each panel),\nand after 2nd order shimming (right)'));
-h = colorbar; h.TickLabels{end} = 'Hz'; % h.Label.String = 'B0 field (Hz)';
-
-
-
+f0masked = f0[mask];
