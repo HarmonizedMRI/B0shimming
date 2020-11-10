abb95f064635b7e15bbf5fb9e9d7eae49c8da8f0
jon <jfnielse@umich.edu>
2020-11-10 14:53:54 -0500

    On branch gradloss 	modified:   shimmodel.jl

--- examples/shimmodel.jl
+++ examples/shimmodel.jl
@@ -3,40 +3,28 @@ using LinearAlgebra
 """
    shimmodel(r, A, s)
 
-Implements f = HAs at spatial location `r`, i.e., 
+Implements f = HAs at spatial locations `r`, i.e., 
 calculates the fieldmap (Hz) at `r` produced by shim settings `s`.
 
-This function is overloaded and also accepts a vector of spatial location vectors.
-
 # Inputs
-- `r`   vector of length 3     Spatial position (cm) (distance from scanner iso-center)
+- `r`   [N 3]                  Spatial locations (cm) (distance from scanner iso-center)
 - `A`   [9 9]                  Shim calibration matrix (includes the B0 term)
-- `s`   vector of length 9     Shim settings (scanner-dependent hardware units)
+- `s`   length-9 vector        Shim settings (scanner-dependent hardware units)
 
 # Output
-- `f`   scalar                 fieldmap (Hz) at spatial location `r`
+- `f`   length-N vector        fieldmap (Hz) at locations `r`
 """
 function shimmodel(
-	r::Vector{<:Real}, 
+	r::Array{<:Real,2},
 	A::Array{<:Real,2}, 
-	s::Vector{<:Real})
+	s::Vector{<:Real}
+	)
 
-	(x,y,z) = r;
-	h = [1 x y z z.^2 x.*y z.*x x.^2-y.^2 z.*y];
+	N = size(r,1)
 
-	return dot(h, A, s)   
-end
+	(x,y,z) = (r[:,1], r[:,2], r[:,3])
 
+	h = [ones(length(x)) x y z z.^2 x.*y z.*x x.^2-y.^2 z.*y];
 
-"""
-   shimmodel(r, A, s)
-
-Acceps a vector of location vectors.
-"""
-function shimmodel(
-	r::Vector{Vector{Float64}},    # TODO: why doesn't <:Real work here?
-	A::Array{<:Real,2}, 
-	s::Vector{<:Real})
-
-	return map((r) -> shimmodel(r,A,s), r)  
+	return [dot(h1, A, s) for h1 in eachrow(h)]
 end
