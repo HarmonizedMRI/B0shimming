8787102212f6c45b40de34e5ab35b40342f582b1
jon <jfnielse@umich.edu>
2020-11-14 17:02:46 -0500

    Loop through shims and look at residuals. On branch gradloss 	modified:   shimdemoWLS.m

--- examples/shimdemoWLS.m
+++ examples/shimdemoWLS.m
@@ -5,13 +5,45 @@
 % Goal is to calculate shim setting changes s that minimize the weighted RMS B0 inhomogeneity.
 % Provided here as an example; other loss functions may be more useful depending on the application.
 
-% createexampledata; 
+% spherical harmonic basis degree
+l = 6;
 
-load exampledata   % A f0 X Y Z mask
+%% Calculate calibration matrix A
+% This only needs to be done once per scanner
 
-% get shim amplitudes 'shat' that minimize RMS fieldmap
+% load calibration data
+load Ffull                             % [nx ny nz 8]  (does not include DC offset term)
+[nx, ny, nz, nShim] = size(Ffull);
+fov = 19.8*[1 1 1];   % cm
+
+% generate mask (only for purpose of calculating A)
+mask = sum(abs(Ffull),4) > 10;         % [nx ny nz]     
+N = sum(mask(:));
+
+% create F = [N 8] by applying mask 
+F = zeros(N, nShim);
+for ii = 1:nShim
+	tmp = Ffull(:,:,:,ii);
+	F(:,ii) = tmp(mask); 
+end
+
+% specify shim amplitudes (differences) used to obtain F (hardware units)
+S = diag([20*ones(1,3) 100*ones(1,5)]);  % Do not include DC term. Hardware units (2nd order unit = 10 mA)
+
+% Get spherical harmonic basis evaluated at the same N spatial locations as F
+[X,Y,Z] = shim.getgrid(nx,ny,nz,fov);
+H = shim.getSHbasis(X(mask),Y(mask),Z(mask),l);   % [N ...]
+
+% Calculate calibration matrix A
+% F and S do NOT include the DC term, i.e., F: [N 8], S: [8 8]
+A = shim.getcalmatrix(F, H, S);
+
+
+%% get shim amplitudes 'shat' that minimize RMS fieldmap
+%shimNum = 5;
+f0 = Ffull(:,:,:,shimNum);  % see how well one shim field fits basis
+mask = sum(abs(Ffull),4) > 10;         % [nx ny nz]
 f0 = f0(mask);   % [N 1]
-N = length(f0);
 W = diag_sp(ones(N,1));
 shat = -(W*H*A)\(W*f0);    % [9 1]. NB! May need to be rounded before applying settings on scanner.
 
@@ -21,9 +53,14 @@ f0 = embed(f0, mask);
 f = embed(f, mask);
 
 % compare baseline and predicted fieldmaps
-im(cat(1, f0, f)); 
-title(sprintf('fieldmaps: original (left in each panel),\nand after 2nd order shimming (right)'));
+subplot(121)
+im(f0); 
+%title(sprintf('fieldmaps: original (left in each panel),\nand after 2nd order shimming (right)'));
+title(sprintf('before shimming'));
 h = colorbar; h.TickLabels{end} = 'Hz'; % h.Label.String = 'B0 field (Hz)';
-
+subplot(122)
+im(f,[-5 5]); 
+title(sprintf('Residual (shim term %d)', shimNum));
+h = colorbar; h.TickLabels{end} = 'Hz';
 
 
