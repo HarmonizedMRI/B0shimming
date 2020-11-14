343d913451ab0b6381f6f128b5b07e5a2b9f46eb
jon <jfnielse@umich.edu>
2020-11-14 15:28:56 -0500

    On branch gradloss 	modified:   createexampledata.m

--- examples/createexampledata.m
+++ examples/createexampledata.m
@@ -12,6 +12,9 @@
 
 addpath ..   % path to +shim package
 
+% spherical harmonic basis degree
+l = 4;
+
 %% Calculate calibration matrix A
 % This only needs to be done once per scanner
 
@@ -36,11 +39,12 @@ S = diag([20*ones(1,3) 100*ones(1,5)]);  % Do not include DC term. Hardware unit
 
 % Get spherical harmonic basis evaluated at the same N spatial locations as F
 [X,Y,Z] = shim.getgrid(nx,ny,nz,fov);
-H = shim.getSHbasis(X(mask),Y(mask),Z(mask));   % [N 9]
+H = shim.getSHbasis(X(mask),Y(mask),Z(mask),l);   % [N 2l+1]
 
 % Calculate calibration matrix A
 % F and S do NOT include the DC term, i.e., F: [N 8], S: [8 8]
 A = shim.getcalmatrix(F, H, S);
+
 
 %% synthesize noisy fieldmap f0 
 nx = 60; ny = 60; nz = 20;
@@ -51,7 +55,7 @@ R = sqrt(X.^2 + Y.^2 + Z.^2);
 mask = ones(size(X));
 mask(R > 0.8*fov(1)/2) = 0;
 mask = logical(mask);
-H = shim.getSHbasis(X(mask),Y(mask),Z(mask));   % [N 9]
+H = shim.getSHbasis(X(mask),Y(mask),Z(mask),l);   % [N 2l+1]
 s(1:4) = 10*randn([1 4]);
 s(5:9) = 20*randn([1 5]);
 R = sqrt(abs(X(mask).^2+Y(mask).^1.5)); 
