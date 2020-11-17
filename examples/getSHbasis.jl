6f92c5be33e34e077827cc10df55499df73c848e
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2020-11-17 11:30:03 -0500

    WIP On branch gradloss 	modified:   getSHbasis.jl

--- examples/getSHbasis.jl
+++ examples/getSHbasis.jl
@@ -1,10 +1,6 @@
 
-using LinearAlgebra: norm, opnorm
-using Flux
-using MIRT: embed!
-using Random: seed!
-using LaTeXStrings
-using Plots; default(markerstrokecolor=:auto)
+using StaticArrays: SVector
+using CoordinateTransformations: SphericalFromCartesian
 
 """
 	function getSHbasis(x,y,z,l)
@@ -25,24 +21,47 @@ function getSHbasis(
 	l::Int = 4
 )
 
-% Spherical coordinate system as defined in https://en.wikipedia.org/wiki/Spherical_harmonics
-% and Romeo and Hoult MRM 1984
-[ph,el,r] = cart2sph(x,y,z);  % ph = azimuth, el = elevation, r = radius
-th = pi/2 - el; % polar angle
+	# convert to spherical coordinates
+	N = length(x)
+	p = Vector{Vector{Float64}}(Float64, N)
+	for ii = 1:N
+		p[ii] = [x(ii], y(jj), z(jj)]
+	end
+	
+	%r = SMatrix{N,3}([x y z])
 
-% construct basis matrix H
-H = zeros(size(X,1), sum(2*(0:l)+1));
-ic = 1;
-for l1 = 0:l
-	lp = legendre(l1, cos(th));   % [l1+1 N]
-	for m = 0:l1
-		f = r.^l1 .* exp(1i*m*ph) .* lp(m+1,:)';
-		H(:,ic) = real(f);
-		ic = ic+1;
-		if m > 0
-			H(:,ic) = imag(f);
+	cart2sph = SphericalFromCartesian()
+
+	# Spherical coordinate system as defined in https://en.wikipedia.org/wiki/Spherical_harmonics
+	# and Romeo and Hoult MRM 1984
+	a = cart2sph.(p);     # a = (radius, azimuth, elevation)
+	r = a.r          # radius
+	θ = π/2 - a.ϕ    # polar angle 
+	ϕ = a.θ          # azimuth
+
+	# construct basis matrix H
+	H = zeros(size(x,1), sum(2*(0:l)+1));
+	ic = 1;
+	for l1 = 0:l
+		lp = legendre(l1, cos(θ));   % [l1+1 N]
+		for m = 0:l1
+			f = r.^l1 .* exp(1i*m*ϕ) .* lp(m+1,:)';
+			H(:,ic) = real(f);
 			ic = ic+1;
+			if m > 0
+				H(:,ic) = imag(f);
+				ic = ic+1;
+ 			end
  		end
- 	end
+	end
+
 end
 
+function getSHbasis(
+	x::Real,
+	y::Real,
+	z::Real,
+	l::Int = 4
+)
+
+end
