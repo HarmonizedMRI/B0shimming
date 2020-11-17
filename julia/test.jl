185a38c54f99de2074ed3fdb740f44c03844f9d5
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2020-11-17 15:59:59 -0500

    On branch gradloss 	modified:   test.jl

--- julia/test.jl
+++ julia/test.jl
@@ -1,17 +1,39 @@
-f(x,y,z) = [x y z]
+function f(x,y,z) 
+	x^2, y^2, z^2
+end
+
 @show f(1,2,3)
 @show map(f, 1, 2, 3)
 @show map(f, [1 2], [2 3], [3 4])
-@show map(f, [1, 2], [2, 3], [3, 4])
+@show a = map(f, [1, 2], [2, 3], [3, 4])
 
 using CoordinateTransformations: SphericalFromCartesian
 
 	c2s = SphericalFromCartesian()
 
-	function cart2sph(x,y,z)
+	function getr(x,y,z)
 		a = c2s([x,y,z]);
-		(a.r, a.θ, π/2-a.ϕ)   # [radius azimuth polar-angle]
+		#(a.r, a.θ, π/2-a.ϕ)   # [radius azimuth polar-angle]
+		a.r
 	end
+
+	function getϕ(x,y,z)
+		a = c2s([x,y,z]);
+		a.θ                    # azimuth
+	end
+
+	function getθ(x,y,z)
+		a = c2s([x,y,z]);
+		π/2-a.ϕ                # polar angle
+	end
+
+	x = [1., 0, 0]
+	y = [0., 1, 0]
+	z = [0., 0, 1]
+
+	r = map((x,y,z) -> getr(x,y,z), x, y, z)
+	ϕ = map((x,y,z) -> getr(x,y,z), x, y, z)
+	r = map((x,y,z) -> getr(x,y,z), x, y, z)
 
 #	x = [0
 #	@show map(cart2sph(x,y,z), x, y, z)
