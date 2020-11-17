316bb0ded4a57854db4c6d2497c3efcc2a0d5118
jon <jfnielse@umich.edu>
2020-11-17 18:16:40 -0500

    On branch gradloss 	modified:   getSHbasis.jl

--- julia/getSHbasis.jl
+++ julia/getSHbasis.jl
@@ -1,6 +1,6 @@
 
 using CoordinateTransformations: SphericalFromCartesian
-using Jacobi: legendre
+using SphericalHarmonics: computeYlm
 
 """
 	function getSHbasis(x,y,z,l)
@@ -9,7 +9,7 @@ Evaluate spherical harmonic basis functions up to degree 'l' at locations [x y z
 
 Inputs:
   x/y/z    length-N vector     x/y/z coordinates at which to evaluate (discretize) the basis (cm)
-  l        int                 SH degree. Default: 4.
+  l        int                 SH degree. Default: 2.
 
 Output:
   H       [N ...]    SH basis, including dc (B0 offset) term
@@ -18,33 +18,45 @@ function getSHbasis(
 	x::Vector{<:Real},
 	y::Vector{<:Real},
 	z::Vector{<:Real},
-	L::Int = 4
+	L::Int = 2
 )
 
-	# Function evaluates spherical harmonic of (degree,order) = (l,m) at position (x,y,z)
 	c2s = SphericalFromCartesian()
-	function sh(x,y,z)              # l,m defined outside function
+	function sh(x,y,z,l,m)
+		# Evaluate spherical harmonic of (degree,order) = (l,m) at position (x,y,z)
 		a = c2s([x,y,z]);
-		r = a.r                      # radius
-		ϕ = a.θ                      # azimuth
-		θ = π/2-a.ϕ                  # polar angle
-		r^l * exp(im*m*ϕ) * legendre(cos(θ),l)
+		(r,ϕ,θ) = (a.r, a.θ, π/2-a.ϕ)    # (radius, azimuth, colatitude)
+		ylm = computeYlm(θ, ϕ, lmax = l)
+		# plm = computePlmcostheta(θ,lmax = l)
+		#r^l * exp(-im*m*ϕ) * plm[(l,m)]
+		r^l * ylm[(l,m)]
 	end
 
 	# construct basis matrix H
-	H = zeros(size(x,1), sum(2*(0:L)+1));
+	H = zeros(size(x,1), sum(2*(0:L) .+ 1));
 	ic = 1;
 	for l = 0:L
 		for m = -l:l
-			@show f = map(sh, x, y, z)
-			#H(:,ic) = real(f);
-			#ic = ic+1;
-			#if m > 0
-			#	H(:,ic) = imag(f);
-			#	ic = ic+1;
- 			#end
+			f = map((x,y,z) -> sh(x,y,z,l,m), x, y, z)
+			H[:,ic] = real(f);
+			ic = ic+1;
  		end
 	end
 
+	H
+
 end
 
+function meshgrid(x, y, z)
+	X = [i for i in x, j in y, k in z]
+	Y = [j for i in x, j in y, k in z]
+	Z = [k for i in x, j in y, k in z]
+	return X, Y, Z
+end
+
+# test function
+function getSHbasis(str::String)
+	(x, y, z) = meshgrid(
+	x = [(i-nx/2)/(nx/2) for i = 1:nx, j = 1:ny, z = 1:nz]
+
+end
