b0dd792d69ddd6a10ec6f8e178d8d167144f287d
jon <jfnielse@umich.edu>
2020-11-18 19:51:22 -0500

    On branch gradloss 	new file:   getcalmatrix.jl

---
+++ julia/getcalmatrix.jl
@@ -0,0 +1,19 @@
+using LinearAlgebra
+
+function getcalmatrix(
+	F::Array{<:Real,2},
+	H::Array{<:Real,2},
+	S::Array{<:Real,2}
+	)
+
+	# Insert DC terms
+	N = size(F,1)
+	F = [ones(N) F] 
+	Sin = S;
+	S = zeros(9);
+	S[1,1] = 1;
+	S[2:end,2:end] = Sin;
+
+	# Calculate calibration matrix A
+	A = inv(H'*H)*H'*F*inv(S)
+end
