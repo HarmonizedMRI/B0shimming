dea174998ad1ee0ee107afe2b01cbd492a6b8765
jon <jfnielse@umich.edu>
2020-11-18 19:36:57 -0500

    On branch gradloss 	modified:   getSHbasis.jl

--- julia/getSHbasis.jl
+++ julia/getSHbasis.jl
@@ -8,11 +8,8 @@ function getSHbasis(
 	L::Int64
 	)
 
-% Spherical coordinate system as defined in https://en.wikipedia.org/wiki/Spherical_harmonics
-% and Romeo and Hoult MRM 1984
-[ph,el,r] = cart2sph(X,Y,Z);  % ph = azimuth, el = elevation, r = radius
-th = pi/2 - el; % polar angle
-
+	# Spherical coordinate system as defined in https://en.wikipedia.org/wiki/Spherical_harmonics
+	# and Romeo and Hoult MRM 1984
 	c2s = SphericalFromCartesian()
 	function sh(x, y, z, l, m)
 		a = c2s([x,y,z])
@@ -21,19 +18,18 @@ th = pi/2 - el; % polar angle
 		Ylm[(l,m)]
 	end
 
-% construct basis matrix H
-H = zeros(size(X,1), sum(2*(0:l)+1));
-ic = 1;
-for l1 = 0:l
-	lp = legendre(l1, cos(th));   % [l1+1 N]
-	for m = 0:l1
-		f = r.^l1 .* exp(1i*m*ph) .* lp(m+1,:)';
-		f = 
-		H(:,ic) = real(f);
-		ic = ic+1;
-		if m > 0
-			H(:,ic) = imag(f);
-			ic = ic+1;
+	# construct basis matrix H
+	H = zeros(size(x,1), sum(2*(0:L) .+ 1))
+	ic = 1
+	for l = 0:L
+		for m = 0:l1
+			f = map( (x,y,z) -> sh(x, y, z, l, m), x, y, z)
+			H(:,ic) = real(f)
+			ic = ic+1
+			if m > 0
+				H(:,ic) = imag(f)
+				ic = ic+1
+ 			end
  		end
  	end
 end
