650f1aafa09d84d65b3aad0e89e3a2a044a02759
jon <jfnielse@umich.edu>
2020-12-08 10:30:12 -0500

    WIP On branch gradloss 	modified:   shimoptim.jl

--- julia/shimoptim.jl
+++ julia/shimoptim.jl
@@ -1,9 +1,33 @@
+using NLopt
 
-function shimoptim(s0::Vector, WHA::Array, Wf0::Vector) # , max_lin::Real, max_hos::Real, max_sum_hos::Real)
+function loss(s::Vector, HA::Array, f0::Vector, shimlims::Tuple)
+	
+	(lin_max, hos_max, hos_sum_mxa) = shimlims
 
-	opt = ADAM(0.4)
-	niter = 300
-	ahos_max = 4000
+	shos = hos_max * atan.(s[5:9]) * 2/pi
+
+	sa = vcat(s[1:4], shos)
+
+   1/2 * norm(HA*sa + f0)^2
+end
+
+
+"""
+s0: initial guess
+HA: Basis H times calibration matrix A
+f0: Acquired field map
+shimlims[1]: max shim current on each linear shim coil
+shimlims[2]: max shim current on each high-order shim coil
+shimlims[3]: max combined high-order shim current
+"""
+function shimoptim(s0::Vector, HA::Array, f0::Vector, shimlims::Tuple)
+
+	opt = Opt(:LD_MMA, length(s))
+	opt.lower_bounds = vcat(-Inf, -shimlims[1]*ones(3,), -shimlims[2]*ones(5,))
+	opt.upper_bounds = vcat(Inf,   shimlims[1]*ones(3,),  shimlims[2]*ones(5,))
+	opt.xtol_rel = 1e-4
+
+	opt.min_objective = loss
 
 	(shat, out) = ls_adam(WHA, Wf0; s0=s0, opt=opt, niter=niter)
 
