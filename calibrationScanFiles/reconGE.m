accde915aae461e7acd0513c3793efc077667b68
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2020-12-10 16:04:26 -0500

    On branch develop 	modified:   reconGE.m

--- calibrationScanFiles/reconGE.m
+++ calibrationScanFiles/reconGE.m
@@ -1,4 +1,4 @@
-function reconGE(dat, readoutFile, imsize, deltaTE)
+function F = reconGE(dat, readoutFile, imSize, deltaTE)
 %
 % Example usage:
 % >> dat = loadScanArchive('ScanArchive_7347633TMRFIX_20201210_120317865.h5');
@@ -13,23 +13,35 @@ nz = imSize(3);
 
 % crop out readout plateau
 [~,~,~,~,~,paramsint16] = toppe.readmod(readoutFile);
-daqStart = paramsint16(1) + 1;
-daqStop = daqStart + paramsint16(2)-1;
+daqStart = paramsint16(1) + 1
+daqStop = daqStart + paramsint16(2)-1
 dat = dat(daqStart:daqStop, :, :, :);
 
 ndat = size(dat, 1);
 ncoils = size(dat, 2);
+nframes = size(dat, 3);
+nechoes = length(deltaTE);
 
-for ie = 1:length(deltaTE)
-	offset = (ie-1)*ny*nz;
-	d = dat(:, :, (offset+1):(offset+nz*ny) );
-	d = reshape(d, [ndat ncoils ny nz]);
-	d = permute(d, [1 3 4 2]);                 % [ndat ny nz ncoils]
+d = zeros(ndat, ncoils, ny, nz, nechoes);
 
-	for c = 1:ncoils
-		imtmp = fftshift(ifftn(fftshift(d(:,:,:,c))));
-		ims(:,:,:,c) = imtmp((2*nx-nx/2):(2*nx+nx/2-1), :, :);
+ifr = 1;    % frame number
+for iz = 1:nz
+	for iy = 1:ny
+		for ie = 1:nechoes
+			d(:,:,iy,iz,ie) = dat(:,:,ifr);
+			ifr = ifr + 1;
+		end
 	end
-	imsos(:,:,:,ie) = sqrt(sum(abs(ims).^2,4));
 end
 
+d = permute(d, [1 3 4 2 5]);      % [ndat ny nz ncoils nechoes]
+
+ims = zeros(nx, ny, nz, ncoils, nechoes);
+for ie = 1:nechoes
+	for c = 1:ncoils
+		imtmp = fftshift(ifftn(fftshift(d(:,:,:,c,ie))));
+		ims(:,:,:,c,ie) = imtmp((2*nx-nx/2):(2*nx+nx/2-1), :, :);
+	end
+end
+
+F = ims;
