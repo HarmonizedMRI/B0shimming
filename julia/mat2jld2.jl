9457fc4ce2cb42827fb2764c99f32a650038c972
jon <jfnielse@umich.edu>
2020-12-11 09:58:57 -0500

    On branch gradloss 	modified:   mat2jld2.jl

--- julia/mat2jld2.jl
+++ julia/mat2jld2.jl
@@ -4,6 +4,7 @@ matf = matread("CalibrationDataUM10Dec2020.mat");
 F = matf["F"];
 S = matf["S"];
 fov = matf["fov"];
+mask = matf["mask"];
 desc = matf["desc"];
 
-@save "CalibrationDataUM10Dec2020.jld2" F S fov desc
+@save "CalibrationDataUM10Dec2020.jld2" F S fov mask desc
