38f109c2ea596ef386924d068d6b9173354b9115
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-01-03 20:53:40 -0500

    On branch gradloss 	modified:   shimoptim.jl

--- julia/shimoptim.jl
+++ julia/shimoptim.jl
@@ -54,3 +54,37 @@ function shimoptim(HA::Array, f0::Vector, shimlims::Tuple;
 	return mins
 end
 
+"""
+	Optimize shims based on loss involving fieldmap gradient
+"""
+function shimoptim(dHxA::Array, dHyA::Array, dHzA::Array, g0x::Vector, g0y::Vector, g0z::Vector, shimlims::Tuple, loss::Function; 
+	s0::Vector = zeros(size(HxA,2),),
+	ftol_rel = 1e-5,
+	cflim = 2000    # empirical observation: needs to be finite for numerical stability
+	)
+
+	(lin_max, hos_max, hos_sum_max) = shimlims
+
+	opt = Opt(:LN_COBYLA, length(s0))
+	opt.ftol_rel = ftol_rel
+	opt.min_objective = (s, grad) -> loss(s, dHxA, dHyA, dHzA, g0x, g0y, g0z)
+
+	# shim current limits (box constraints)
+	if length(s0) > 4
+		opt.lower_bounds = [-cflim; -lin_max; -hos_max]
+		opt.upper_bounds = [ cflim;  lin_max;  hos_max]
+	else
+		opt.lower_bounds = [-cflim; -lin_max]
+		opt.upper_bounds = [ cflim;  lin_max]
+	end
+
+	# limit on total HO shim current
+	if ~isempty(hos_sum_max) & length(s0) > 4
+		inequality_constraint!(opt, (s, grad) -> sum(abs.(s[5:end])) - hos_sum_max)
+	end
+
+	(minf,mins,ret) = optimize(opt, s0)
+
+	return mins
+end
+
