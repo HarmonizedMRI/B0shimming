c4281cdd9c5040b8db018a1befb7d7fdd1d66244
jon <jfnielse@umich.edu>
2021-01-24 09:07:39 -0500

    On branch gradloss 	modified:   makefig_yogesh.m

--- julia/makefig_yogesh.m
+++ julia/makefig_yogesh.m
@@ -1,17 +1,25 @@
 
 load result_p2_l6.mat
+f0 = flipdim(permute(f0, [2 1 3]),2);
+fp = flipdim(permute(fp, [2 1 3]),2);
+g0 = flipdim(permute(g0, [2 1 3]),2);
+gp = flipdim(permute(gp, [2 1 3]),2);
 gp_p2_l6 = gp .* mask;
 fp_p2_l6 = fp .* mask;
 
 load result_p10_l6.mat
+f0 = flipdim(permute(f0, [2 1 3]),2);
+fp = flipdim(permute(fp, [2 1 3]),2);
+g0 = flipdim(permute(g0, [2 1 3]),2);
+gp = flipdim(permute(gp, [2 1 3]),2);
 gp_p10_l6 = gp .* mask;
 fp_p10_l6 = fp .* mask;
 
 g0 = g0 .* mask;
 f0 = f0 .* mask;
 
+y = 9:56;
 x = 7:56;
-y = 7:56;
 z = 3;
 
 g0 = g0(x,y,z);
