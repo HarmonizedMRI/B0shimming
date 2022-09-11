6173798dd347273fb62662448d381f843f37dc40
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-09-10 23:13:56 -0400

    On branch develop 	modified:   loadmat.jl

--- julia/loadmat.jl
+++ julia/loadmat.jl
@@ -1,7 +1,7 @@
 using MAT
 
 # Load shim calibration data
-matf = matread("shimCal.mat");
+matf = matread("shimcal.mat");
 F = matf["F"]   # [nx ny nz 8] for 2nd order shim systems
 S = matf["S"]   # [8 8] matrix with shim amplitudes (typically diagonal)
 maskcal = matf["mask"];
