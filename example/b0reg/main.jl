b0f63a1a046916dd06785c01f804d0d141e08af8
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-01-08 18:11:35 -0500

    On branch example 	modified:   b0reg/main.jl 	new file:   getb02.m

--- example/b0reg/main.jl
+++ example/b0reg/main.jl
@@ -7,8 +7,11 @@ images = ComplexF32.(images) # 32-bit floats saves memory and thus time
 matf = matread("echotime.mat")
 tmp = matf["echotime"]
 echotime = [tmp[1], tmp[2]]  # vector
+matf = matread("../sens.mat")
+sens = matf["sens"]   # [nx ny ... ncoil nechotime]
+sens = ComplexF32.(sens) # 32-bit floats saves memory and thus time
 
-b0, _, _ = b0map(images, echotime) # regularized fieldmap in Hz
+b0, _, _ = b0map(images, echotime; smap=sens) # regularized fieldmap in Hz
 
 # Write to .mat file for viewing
 matwrite("b0.mat", Dict(
