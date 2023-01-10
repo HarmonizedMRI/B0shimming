7db89c34a853242c25d6b802acbed44797257259
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-01-10 14:23:05 -0500

    On branch example 	modified:   main.m

--- example/main.m
+++ example/main.m
@@ -1,6 +1,6 @@
 
 % create shimcal.mat (F S mask FOV)
-makeshimcal;  % edit as needed to ponit to shim calibration Pfiles
+%makeshimcal;  % edit as needed to ponit to shim calibration Pfiles
 
 % b0init, mask, x1/x2
 getb0init;    % edit as needed to point to b0 acquistion
@@ -10,3 +10,10 @@ makef0;       % regularized version of b0init. Uses mask from getb0init.
 
 % create shimvol.mat (mask) by skull stripping x1
 makeshimvol;
+
+% Calculate shims
+% $ cd ~/github/HarmonizedMRI/B0shimming/julia
+% $ julia
+% pkg> activate .
+% pkg> instantiate
+% julia> include("shim.jl");
