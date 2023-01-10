3625746b1c612d8b07fe3d8d13df29adfc1b80c6
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-01-10 13:56:56 -0500

    On branch example 	modified:   makeshimvol.m

--- example/makeshimvol.m
+++ example/makeshimvol.m
@@ -3,14 +3,22 @@
 
 m = abs(x1);  % magnitude image
 
-% flips so the output of bet has the original orientation
+% flip dimensions to match expected orientation for nii images
 m = flipdim(m, 1);
 m = flipdim(m, 2);
 m = flipdim(m, 3);
+
 niftiwrite(m, 'in.nii');
 
 input('do: \`bet in.nii out.nii.gz -f 0.4 -m\`, then press any key to continue');
 
+mb = niftiread('out.nii.gz');
+mb = flipdim(mb, 1);
+mb = flipdim(mb, 2);
+mb = flipdim(mb, 3);
 mask = niftiread('out_mask.nii.gz');
+mask = flipdim(mask, 1);
+mask = flipdim(mask, 2);
+mask = flipdim(mask, 3);
 
 save shimvol.mat mask
