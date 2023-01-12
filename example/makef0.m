bb3441c136bc59e4cb44ce8811b73d6adb443504
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-01-12 17:06:13 -0500

    On branch example 	new file:   getfiles 	modified:   makef0.m

--- example/makef0.m
+++ example/makef0.m
@@ -1,22 +1,26 @@
 % Regularize B0 map
-%
 
-% regularize field map
-% NB! Input to mri_field_map_reg is rad/sec
-yik(:,:,:,1) = magraw;
-yik(:,:,:,2) = magraw .* exp(1i*2*pi*b0init*dte);
-l2b = -1;
-fprintf('Regularizing field map...');
-load mask  % from getb0init.m
 tic
-[wmap, wconv] = mri_field_map_reg(yik, [0 dte], ...
-    'mask', mask, 'winit', b0init*2*pi, 'l2b', l2b);
-fprintf(' done\n');
-toc
-wmap = wmap .* mask;   % rad/sec
+if true
+    % regularize field map
+    % NB! Input to mri_field_map_reg is rad/sec
+    yik(:,:,:,1) = magraw;
+    yik(:,:,:,2) = magraw .* exp(1i*2*pi*b0init*dte);
+    l2b = -1;
+    fprintf('Regularizing field map...');
+    load mask  % from getb0init.m
+    [wmap, wconv] = mri_field_map_reg(yik, [0 dte], ...
+        'mask', mask, 'winit', b0init*2*pi, 'l2b', l2b);
+    fprintf(' done\n');
+    wmap = wmap .* mask;   % rad/sec
 
-% final b0 estimate
-b0 = wmap / (2*pi);  % Hz
+    % final b0 estimate
+    b0 = wmap / (2*pi);  % Hz
+else
+    input('Run b0reg/main.jl, then press any key to continue');
+    load b0reg/b0;
+end
+toc 
 
 f0 = b0;
 
