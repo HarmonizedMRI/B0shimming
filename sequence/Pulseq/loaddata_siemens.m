93dc13f5922acc9732a2b66c9efb95ee0da3bc3a
Nikbert <niklas.wehkamp@uniklinik-freiburg.de>
2023-12-22 11:14:10 +0100

    clean up loaddata_siemens

--- sequence/Pulseq/loaddata_siemens.m
+++ sequence/Pulseq/loaddata_siemens.m
@@ -12,25 +12,24 @@ function d = loaddata_siemens(data_path)
 
 % load data from .dat-file
 twix = mapVBVD(data_path);
+% twix = data_path;
 
-% twix.image.flagDoAverage = true; %???
 twix.image.flagRemoveOS  = false; % remove OverSampling
 % twix.image.flagRemoveOS  = true; % remove OverSampling
 
 
-% data = squeeze(twix.image());
 data_unsorted = twix.image.unsorted();
 din = permute(data_unsorted, [1 3 2]);   % [nfid nview nslice ncoil]
 % twix.image.dataSize()
 
-
 % discard data during receive gain calibration (see writeB0.m)
 % din = din(:,:,2:end,:);  
+
 % """ remove dummy shots """
-nzDummy = 1; %2; %# should be reduced to 1 in future
+nzDummy = 1;
 nx = size(din,1); 
 ny = nx; %twix.hdr.Meas.ImageColumns, twix.hdr.Meas.ImageLines
-nz = nx; %#attention hard coded shit!!!
+nz = nx; %#!!!
 nCoils = size(din,3);
 
 nTE = 2;   % number of echo times (interleaved)
@@ -43,54 +42,4 @@ end
 d = reshape(d, [nx ny nz nCoils nTE]);
 
 % display center kz encode
-figure,imagesc(abs(squeeze(d(:,:,30,1,1))).^0.2);
-
-% im(abs(d(:,:,end/2,1,1)).^0.2)
-% [nfid,ncoil,nslice,necho,nview] = size(din);   % necho = 1 by construction
-
-% nRead = 2*nx; %#attention hard coded shit!!!
-% nRead = 2*nx/2; %#attention hard coded shit!!!
-
-% %% first delete Dummy then reshape as is loaddata_ge...
-% nDummyShots = nzDummy * (nx + ny)+1; %???+1 hm probably not *nCoils
-% din = din(:, nDummyShots:end, :); %# [nFid 2*ny*nz nCoils]
-% din = reshape(din,[nx,nz,2*ny,nCoils]); %???? %
-% d(:,:,:,:,1) = din(:,:,1:2:end,:);   % TE1 data, size [60 60 60]???
-% d(:,:,:,:,2) = din(:,:,2:2:end,:);   % TE2 data, size [60 60 60]???
-%% last week
-% % data = reshape(data,[nx,ny*2,nz,nCoils]); %???? %
-% 
-% % % % data = reshape(data,[nRead,2*ny,nz,2]); %???? %
-% % % din = data;
-% 
-% % And here's the k-space for the first coil and the first slice:
-% % figure,imagesc(abs(squeeze(data(:,:,30,1))).^0.2);
-% 
-% % din = flipdim(din, 1);   % TOPPE data is flipped along first dimension
-% % din = flipdim(din, 1);   % TOPPE data is flipped along first dimension
-% 
-% % din = reshape(din,[nx,ny,2*(nz+1),nCoils]); %???? %
-% din = reshape(din,[nx,nz+1,2*ny,nCoils]); %???? %
-% 
-% % din = reshape(din,[nx,2*(nz+nzDummy),ny,nCoils]); %???? %
-% 
-% % din = permute(din, [1 5 3 2 4]);   % [nfid nview nslice ncoil]
-% % 
-% % % discard data during receive gain calibration (see writeB0.m)
-% % din = din(:,:,2:end,:);  %at GE
-% % din = din(:,:,3:end,:); 
-% din = din(:,2:end,:,:); 
-% % din = din(:,3:end,:,:); 
-% 
-% % din = din(:,:,2:end,:); ????
-% 
-% % construct output matrix
-% % [nx ny nz nCoils] = size(din(:,1:2:end,:,:));
-% % d = zeros(nx, ny, nz, nCoils, 2);
-% % d(:,:,:,:,1) = din(:,1:2:end,:,:);   % TE1 data, size [60 60 60]???
-% % d(:,:,:,:,2) = din(:,2:2:end,:,:);   % TE2 data, size [60 60 60]???
-% d(:,:,:,:,1) = din(:,:,1:2:end,:);   % TE1 data, size [60 60 60]???
-% d(:,:,:,:,2) = din(:,:,2:2:end,:);   % TE2 data, size [60 60 60]???
-
-% d = permute(din, [1,3,4,5,2]);
-% d = permute(din, [1,2,4,5,3]);
+im(abs(d(:,:,end/2,1,1)).^0.2)
