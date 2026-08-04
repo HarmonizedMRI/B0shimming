beb744318a606493646a42bf0e05d2f68c8cb47e
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2026-08-04 19:22:17 -0400

    On branch recon 	renamed:    reconstruction/main.m -> reconstruction/matlab/main.m 	renamed:    reconstruction/setup.m -> reconstruction/matlab/setup.m

--- reconstruction/matlab/main.m
+++ reconstruction/matlab/main.m
@@ -15,7 +15,9 @@ n_z_dummy = 1;
 FOV = [24 24 24]*1e-2;     % m
 
 % load GE ScanArchive file and check size
-%d_cell = pge2.utils.loaddata('data.h5');   % cell array
+if ~exist('d_cell', 'var')
+    d_cell = pge2.utils.loaddata('data.h5');   % cell array
+end
 n_shots = length(d_cell);
 assert(n_shots == n_TE * n_y * (n_z_dummy + n_z), ...
     'The number of TRs in data file does not match stated acquisition parameters');
@@ -26,7 +28,7 @@ n_c = size(d_cell{1}, 2);  % number of receive coils
 % sort data into an array
 d = zeros(n_x, n_c, n_TE*n_y, n_z);
 
-shot = n_z_dummy * n_y + 1;   % first shot after dummy shots
+shot = n_z_dummy * n_TE * n_y + 1;   % first shot after dummy shots
 for z = 1 : n_z
     for y = 1 : n_TE*n_y
         d(:, :, y, z) = d_cell{shot};
@@ -36,3 +38,29 @@ end
 
 d = permute(d, [1 3 4 2]);
 
+addpath ../julia/
+fprintf('saving...')
+saveReconstructionInput('reconstruction_input', d, TE); %, options);
+
+return
+
+%
+d_te1 = d(:,1:2:end,:,:);
+d_te2 = d(:,2:2:end,:,:);
+
+% reconstruct coil images
+[im_te1, mag_te1] = hmriutils.recon.ift3(d_te1);
+[im_te2, mag_te2] = hmriutils.recon.ift3(d_te2);
+
+% direct field map calculation
+pc = hmriutils.recon.phasecontrastmulticoil(im_te1, im_te2);
+b0 = pc/2/pi/delta_TE;     % Hz
+
+% crop to match SMS-EPI matrix size
+%b0.raw = imcropmatrix(b0.raw, [nx ny nz]);
+%b0.mag = imcropmatrix(b0.mag, [nx ny nz]);
+
+% direct multi-coil phase-cont
+im(b0)
+
+%save reconstruction im_te1 im_te2
--- reconstruction/matlab/setup.m
+++ reconstruction/matlab/setup.m
@@ -6,6 +6,10 @@ addpath pulseq/matlab
 system('git clone git@github.com:HarmonizedMRI/pge2.git');
 addpath pge2/matlab
 
+% ift3.m
+system('git clone git@github.com:HarmonizedMRI/utils.git');
+addpath utils
+
 % To load the ScanArchive raw data files you will also need the Orchestra toolbox
 % which is available for download at http://weconnect.gehealthcare.com/
 addpath ~/Programs/orchestra-sdk-2.1-1.matlab/
@@ -15,3 +19,6 @@ system('git clone git@github.com:HarmonizedMRI/pulseg.git');
 addpath pulseg/matlab
 addpath(genpath('pulseg/matlab/third_party'));
 
+% get 'im' display function (optional)
+system('git clone --depth 1 git@github.com:JeffFessler/mirt.git');
+cd mirt; setup; cd ..;
